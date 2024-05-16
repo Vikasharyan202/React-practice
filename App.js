4d9682ffb20e48e4adbbc8b1fa2f649e86711ea5
202vikash@gmail.com <vikash_aryan@202vikalcomsAir.Vortex.local>
2024-05-16 11:41:27 +0530

    added all dependencies to project

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading  = React.createElement("h1", {id:"heading", class:"header"}, "Hello, Welcome to the react course");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
