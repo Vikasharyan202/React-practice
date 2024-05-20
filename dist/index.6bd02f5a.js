0259bb782a848959baa378f7d8b4a1f1d51e4229
Vikash Aryan <117556610+Vikasharyan202@users.noreply.github.com>
2024-05-20 11:48:53 +0530

    Delete dist directory

--- dist/index.6bd02f5a.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement("h1", {
-    id: "heading",
-    class: "header"
-}, "Hello, Welcome to the react course");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
